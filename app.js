2fe070a3b67e399fe666ced6d10e3d233e478df1
alirazagpt4 <ali.4aug24webgpt@gmail.com>
2025-01-21 21:05:27 +0500

    changes in projects

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("working");
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+app.get("/" , (req , res)=>{
+    res.send("Welcome to the Express Server!");
+})
+ 
+module.exports = app;
